10a58edebdbb7d8d3dabccfa77ec762c7fc126e6
KonstantinKirsanov <kirsanov.kostya96@gmail.com>
2020-04-29 20:55:59 +0700

    Rewrote first column items code from html to pug

---
+++ src/components/range-slider/index.js
@@ -0,0 +1,16 @@
+$( function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 15000,
+      step: 100,
+      values: [ 5000, 10000 ],
+      slide: function( event, ui ) 
+      
+      {
+        $( "#amount" ).val(ui.values[ 0 ].toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ') + "₽" + " - " +  ui.values[ 1 ].toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ') + "₽");
+      }
+    });
+    $( "#amount" ).val($( "#slider-range" ).slider( "values", 0 ).toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ') + "₽" +
+      " - " + $( "#slider-range" ).slider( "values", 1 ).toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ') + "₽" );
+  } );
