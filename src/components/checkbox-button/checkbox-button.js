37f60e1ba0ec7b9230263c7d4f178a2aa59f8d74
KonstantinKirsanov <kirsanov.kostya96@gmail.com>
2020-05-05 22:36:58 +0700

    Form Elements 95% completed

---
+++ src/components/checkbox-button/checkbox-button.js
@@ -0,0 +1,4 @@
+function expand_checkbox_dropdown() {
+    $("#expand-checkbox-dropdown").toggleClass('disappeared');
+    $("#expand-checkbox-list__span").toggleClass('icons-rotate');
+}
--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -44,12 +44,68 @@ function dropdown_room (btn_id,sign) {
     $(".dropdown__field__total-text").html(x + text_x + y + text_y)
 }
 
-function dropdown_hide (id) {
+function dropdown_hide (id,qw) {
     $(id).toggleClass('disappeared');
-    $('.dropdown-room').toggleClass('box-shadow_none')
+    $(qw).toggleClass('box-shadow_none')
 }
 
-function dropdown_appear (id) {
+function dropdown_appear (id,qw) {
     $(id).toggleClass('disappeared');
-    $('.dropdown-room').toggleClass('box-shadow')
+    $(qw).toggleClass('box-shadow')
+}
+
+
+function dropdown_guest (btn_id,sign) {
+    var count=$("#dropdown-room__text-" + btn_id).text();
+    $("#dropdown_guest_clear_btn").attr("class", "button-3 button-3_grey")
+    if (sign==true)
+        {
+            $("#dropdown-room__text-" + btn_id).html(+$("#dropdown-room__text-" + btn_id).html()+1);
+            $("#dropdown-room__btn-" + btn_id + "_minus").attr("class", "dropdown-btn");
+        }
+    else
+        {
+            if (count>1)
+                $("#dropdown-room__text-" + btn_id).html(+$("#dropdown-room__text-" + btn_id).html()-1);
+            else
+                if (count==1)
+                {
+                    $("#dropdown-room__text-" + btn_id).html(+$("#dropdown-room__text-" + btn_id).html()-1);
+                    $("#dropdown-room__btn-" + btn_id + "_minus").attr("class", "dropdown-btn dropdown-btn_disabled");
+                }  
+        }
+}
+
+function dropdown_guest_aply () {
+    var x=+$("#dropdown-room__text-" + 3).html();
+    var y=+$("#dropdown-room__text-" + 4).html();
+    var z=+$("#dropdown-room__text-" + 5).html();
+    var text= "";
+    var total_count=x+y+z;
+    if (total_count>0)
+        {
+            if (total_count==0 || total_count>4)
+                text=" гостей";
+            else
+            {
+                if (total_count==1)
+                    text=" гость";
+                else
+                    text=" гостя";
+            }   
+            $("#dropdown-guests__total-count").html(total_count + text);
+        }
+    
+    
+}
+
+function dropdown_guest_clear () {
+    $("#dropdown-guests__total-count").html("Сколько гостей") ;
+    $("#dropdown_guest_clear_btn").attr("class", "hidden");
+    $("#dropdown-room__text-" + 3).html("0");
+    $("#dropdown-room__text-" + 4).html("0");
+    $("#dropdown-room__text-" + 5).html("0");
+    $("#dropdown-room__btn-" + 3 + "_minus").attr("class", "dropdown-btn dropdown-btn_disabled");
+    $("#dropdown-room__btn-" + 4 + "_minus").attr("class", "dropdown-btn dropdown-btn_disabled");
+    $("#dropdown-room__btn-" + 5 + "_minus").attr("class", "dropdown-btn dropdown-btn_disabled");
 }
